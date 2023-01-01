9fb17436ed518fab8f74ad04bc74936576bde30a
Vincent-ondeng <vincentondeng@gmail.com>
2023-01-01 13:36:22 +0300

    facilities

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # First install the folium package and create a virtual environment
-# Create a map instance
 
+# Folium for plotting maps
 import folium
-m = folium.Map(location=[-0.15386345206107715, 34.86414568574279], zoom_start=12,
-               control_scale=True, prefer_canvas=True)
+
+
+# Create a map instance
+m = folium.Map(location=[-0.15386345206107715, 34.86414568574279],
+               zoom_start=12,
+               control_scale=True,
+               prefer_canvas=True)
+
+# Adding the Kisumu County Ward Boundaries
+folium.GeoJson('facilities.geojson', name='Facilities').add_to(m)
+
+
+# Adding the tile control layer for the different tiles in Kisumu County
+folium.TileLayer('openstreetmap').add_to(m)
+folium.TileLayer('cartodbdark_matter').add_to(m)
+folium.LayerControl().add_to(m)
 
 # Add marker
 # Run: help(folium.Icon) for more info about icons
@@ -14,4 +28,4 @@ folium.Marker(
     icon=folium.Icon(color='green', icon='ok-sign'),
 ).add_to(m)
 
-m.save('map.html')
+m.save('index.html')
